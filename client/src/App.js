d02a5986957a433d6694c211db23e217823b0f52
shabbirg89 <shabbirg89@gmail.com>
2023-07-15 10:45:59 +0530

    Site Updated version 1

--- client/src/App.js
+++ client/src/App.js
@@ -6,36 +6,37 @@ import video2Thumbnail from './assets/video2_thumbnail.jpg';
 import video3Thumbnail from './assets/video3_thumbnail.jpg';
 import video4Thumbnail from './assets/video4_thumbnail.jpg';
 import video5Thumbnail from './assets/video5_thumbnail.jpg';
+import video6Thumbnail from './assets/video6_thumbnail.jpg';
+import video7Thumbnail from './assets/video7_thumbnail.jpg';
+import video8Thumbnail from './assets/video8_thumbnail.jpg';
+import video9Thumbnail from './assets/video9_thumbnail.jpg';
 
 const playlists = [
   {
     id: 1,
-    name: 'MySQL playlist for Beginners to Advance',
+    name: 'MySQL playlist for Beginners',
     videos: [
       {
         id: 1,
-        title: 'MySQL playlist for Beginners to Advance',
+        title: '42 videos',
         description: `Join our comprehensive MySQL tutorial playlist and master the world of database management. From basic CRUD operations to advanced topics like joins and stored procedures, gain practical skills to design and manage robust databases. Perfect for beginners and professionals alike. Let's dive into MySQL and unlock its full potential together!`,
         thumbnail: video1Thumbnail,
         youtubeLink: 'https://youtu.be/83IHr2fQ2Rs',
-        //githubLink: 'https://github.com/yourname/video1',
       },
       // Add more video objects for the first playlist
     ],
   },
   {
     id: 2,
-    name: 'Python Tutorials for Beginners to Advance',
+    name: 'Python Tutorials for Beginners',
     videos: [
       {
         id: 1,
-        title: 'Python Tutorials for Beginners to Advance',
+        title: '107 videos',
         description: `Embark on a Python programming journey with our comprehensive tutorial playlist. Whether you're a beginner or an experienced programmer, this playlist is tailored to expand your Python skills. From fundamental concepts to advanced topics like object-oriented programming and data manipulation, we cover it all. Get hands-on with practical examples and exercises, empowering you to write efficient and powerful Python code. Join us and unleash the full potential of Python in your programming endeavors. Let's dive into Python together!`,
         thumbnail: video2Thumbnail,
         youtubeLink: 'https://youtu.be/CzgWuaO4iU8',
-        //githubLink: 'https://github.com/yourname/video1',
       },
-    
       // Add more video objects for the second playlist
     ],
   },
@@ -45,14 +46,12 @@ const playlists = [
     videos: [
       {
         id: 1,
-        title: 'Java Tutorials for Beginners to Advance',
+        title: '21 videos',
         description: `Explore the world of Java programming with our comprehensive tutorial playlist. Whether you're new to coding or an experienced developer, this playlist is designed to enhance your Java skills. From the basics of syntax and control flow to advanced topics such as object-oriented programming and exception handling, we've got you covered. Dive into practical examples and exercises that will sharpen your Java proficiency. Join us on this learning journey and unlock the full potential of Java in your programming endeavors. Let's dive into Java together!`,
         thumbnail: video3Thumbnail,
         youtubeLink: 'https://youtu.be/6oEXW_EvPiY',
-        //githubLink: 'https://github.com/yourname/video1',
       },
-    
-      // Add more video objects for the second playlist
+      // Add more video objects for the third playlist
     ],
   },
   {
@@ -61,31 +60,83 @@ const playlists = [
     videos: [
       {
         id: 1,
-        title: 'AS400 Tutorials for Beginners to Advance',
+        title: '29 videos',
         description: `Embark on a journey to master AS400 RPG and COBOL programming with our comprehensive tutorial playlist. Whether you're a beginner or experienced developer, this playlist is tailored to enhance your skills in these powerful languages. From the fundamentals of AS400 RPG and COBOL syntax to advanced topics like file handling and program optimization, we cover it all. Dive into practical examples and exercises to gain hands-on experience and sharpen your abilities. Join us on this learning adventure and unlock the full potential of AS400 RPG and COBOL in your programming endeavors. Let's dive into AS400 RPG and COBOL together!`,
         thumbnail: video4Thumbnail,
         youtubeLink: 'https://youtu.be/6oEXW_EvPiY',
-        //githubLink: 'https://github.com/yourname/video1',
       },
-    
-      // Add more video objects for the second playlist
+      // Add more video objects for the fourth playlist
     ],
   },
   {
     id: 5,
-    name: 'PySPARK Tutorials for Beginners',
+    name: 'PySPARK Tutorials for Beginners to Advance',
     videos: [
       {
         id: 1,
-        title: 'PySPARK Tutorials for Beginners',
+        title: '10 videos',
         description: `
         Embark on a PySpark tutorial playlist and unlock the power of big data processing with Python. Whether you're a beginner or an experienced data professional, this playlist is designed to enhance your PySpark skills. From the basics of distributed computing and data transformation to advanced topics like machine learning and data streaming, we cover it all. Dive into practical examples and exercises to gain hands-on experience with PySpark's powerful capabilities. Join us on this learning journey and harness the full potential of PySpark in your big data projects. Let's dive into PySpark together!`,
         thumbnail: video5Thumbnail,
         youtubeLink: 'https://youtu.be/AB2nUrKYRhw',
-        //githubLink: 'https://github.com/yourname/video1',
       },
-    
-      // Add more video objects for the second playlist
+      // Add more video objects for the fifth playlist
+    ],
+  },
+  {
+    id: 6,
+    name: 'R programming Tutorials for Beginners to Advance',
+    videos: [
+      {
+        id: 1,
+        title: '12 videos',
+        description: `Embark on a comprehensive R programming tutorial playlist and unlock the potential of statistical computing and data analysis. Whether you're a beginner or an experienced data scientist, this playlist is tailored to enhance your R skills. From the basics of data manipulation and visualization to advanced topics like statistical modeling and machine learning, we cover it all. Dive into practical examples and exercises to gain hands-on experience and sharpen your abilities in R programming. Join us on this learning journey and harness the full potential of R in your data-driven projects. Let's dive into R programming together!`,
+        thumbnail: video6Thumbnail,
+        youtubeLink: 'https://youtu.be/cqJ5bgJh1lM',
+      },
+      // Add more video objects for the sixth playlist
+    ],
+  },
+  {
+    id: 7,
+    name: 'MS EXCEL for Beginners',
+    videos: [
+      {
+        id: 1,
+        title: '4 videos',
+        description: `Embark on a comprehensive R programming tutorial playlist and unlock the potential of statistical computing and data analysis. Whether you're a beginner or an experienced data scientist, this playlist is tailored to enhance your R skills. From the basics of data manipulation and visualization to advanced topics like statistical modeling and machine learning, we cover it all. Dive into practical examples and exercises to gain hands-on experience and sharpen your abilities in R programming. Join us on this learning journey and harness the full potential of R in your data-driven projects. Let's dive into R programming together!`,
+        thumbnail: video7Thumbnail,
+        youtubeLink: 'https://youtu.be/hOVxvOFXx3Q',
+      },
+      // Add more video objects for the seventh playlist
+    ],
+  },
+  {
+    id: 8,
+    name: 'ChatGPT/LLM tutorial for Beginners with Python',
+    videos: [
+      {
+        id: 1,
+        title: '8 videos',
+        description: `Embark on a comprehensive Large Language Model (LLM) tutorial playlist and dive into the world of advanced natural language processing and artificial intelligence. Whether you're a researcher, developer, or language enthusiast, this playlist is tailored to enhance your understanding and usage of large language models. From the basics of LLM architecture and training to advanced topics like fine-tuning and transfer learning, we cover it all. Explore practical examples and hands-on exercises to gain expertise in generating human-like text and language understanding. Join us on this learning journey and unlock the full potential of large language models in your AI projects. Let's dive into LLM together!`,
+        thumbnail: video8Thumbnail,
+        youtubeLink: 'https://youtu.be/BB--sfneIaY',
+      },
+      // Add more video objects for the eighth playlist
+    ],
+  },
+  {
+    id: 9,
+    name: 'Github tutorial for Beginners',
+    videos: [
+      {
+        id: 1,
+        title: '5 videos',
+        description: `Embark on a comprehensive Large Language Model (LLM) tutorial playlist and dive into the world of advanced natural language processing and artificial intelligence. Whether you're a researcher, developer, or language enthusiast, this playlist is tailored to enhance your understanding and usage of large language models. From the basics of LLM architecture and training to advanced topics like fine-tuning and transfer learning, we cover it all. Explore practical examples and hands-on exercises to gain expertise in generating human-like text and language understanding. Join us on this learning journey and unlock the full potential of large language models in your AI projects. Let's dive into LLM together!`,
+        thumbnail: video9Thumbnail,
+        youtubeLink: 'https://youtu.be/BB--sfneIaY',
+      },
+      // Add more video objects for the ninth playlist
     ],
   },
   // Add more playlist objects following the same structure
@@ -133,30 +184,79 @@ function App() {
         <h1>SHABBIR GOVERNOR</h1>
       </div>
       <div className="about-me" id="about-me">
-        <h2>About Me</h2>
+        <h2>My Mission:</h2>
         <p>{aboutMe.description}</p>
         <p>Check out my latest videos:</p>
       </div>
-      {playlists.map((playlist) => (
-        <div key={playlist.id}>
-          <h2>{playlist.name}</h2>
-          {playlist.videos.map((video) => (
-            <div key={video.id} className="video-container">
-              <img src={video.thumbnail} alt="Video Thumbnail" />
-              <h3>{video.title}</h3>
-              <p>{video.description}</p>
-              <div className="links">
-                <a href={video.youtubeLink} target="_blank" rel="noopener noreferrer">
-                  Watch on YouTube
-                </a>
-                <a href={video.githubLink} target="_blank" rel="noopener noreferrer">
-                  GitHub
-                </a>
-              </div>
+      <div>
+        <h2>Basic Playlists:</h2>
+        {playlists
+          .filter((playlist) => playlist.id <= 3)
+          .map((playlist) => (
+            <div key={playlist.id}>
+              <h3>{playlist.name}</h3>
+              {playlist.videos.map((video) => (
+                <div key={video.id} className="video-container">
+                  <img src={video.thumbnail} alt="Video Thumbnail" />
+                  <h4>{video.title}</h4>
+                  <p>{video.description}</p>
+                  <div className="links">
+                    <a
+                      href={video.youtubeLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Watch on YouTube
+                    </a>
+                  </div>
+                </div>
+              ))}
             </div>
           ))}
-        </div>
-      ))}
+      </div>
+      <div>
+        <h2>Advanced Playlists:</h2>
+        {playlists
+          .filter((playlist) => playlist.id > 3)
+          .map((playlist) => (
+            <div key={playlist.id}>
+              <h3>{playlist.name}</h3>
+              {playlist.videos.map((video) => (
+                <div key={video.id} className="video-container">
+                  <img src={video.thumbnail} alt="Video Thumbnail" />
+                  <h4>{video.title}</h4>
+                  <p>{video.description}</p>
+                  <div className="links">
+                    <a
+                      href={video.youtubeLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Watch on YouTube
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ))}
+      </div>
+      <footer className="footer">
+        <p>© 2023 Shabbir Governor</p>
+        <p>
+          <a href={aboutMe.linkedin} target="_blank" rel="noopener noreferrer">
+            LinkedIn
+          </a>{' '}
+          |{' '}
+          <a href={aboutMe.github} target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>{' '}
+          |{' '}
+          <a href={aboutMe.youtube} target="_blank" rel="noopener noreferrer">
+            YouTube
+          </a>{' '}
+          | <a href={`mailto:shabbirg89@gmail.com`}>Email</a>
+        </p>
+      </footer>
     </div>
   );
 }
