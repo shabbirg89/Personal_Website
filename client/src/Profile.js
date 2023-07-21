980a75ae6c39dba5fd4ad34506c223223968353b
shabbirg89 <shabbirg89@gmail.com>
2023-07-21 19:14:29 +0530

    Site Updated version 1

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,12 +1,14 @@
 // Profile.js
 import React from 'react';
 import profileImage from './profile.jpg';
+import './Profile.css'; // Import the CSS file for the component
 
 const Profile = ({ name }) => {
   return (
-    <div id="home">
-      <img src={profileImage} alt="Profile" className="profile-image" />
-      <h1>{name}</h1>
+    <div id="home" className="profile-section">
+      <div className="profile-background" />
+      <img src={profileImage} alt="Profile" className="profile-image fade-in" />
+      <h1 className="slide-in">{name}</h1>
     </div>
   );
 };
